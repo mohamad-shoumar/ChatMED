63348263adc3a12563fefcb3256187558dd5ac9b
MoeShoumar <mks38@mail.aub.edu>
2023-04-15 00:00:17 +0300

    created user model

---
+++ backend/src/models/UserModel.ts
@@ -0,0 +1,51 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+import { IMedicalHistory } from "./MedicalHistoryModel";
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  role: string;
+  picture?: string;
+  fullName: string;
+  gender: "male" | "female" | "other";
+}
+
+const UserSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    hashed: true,
+  },
+  role: {
+    type: String,
+    enum: ["patient", "doctor"],
+    required: true,
+  },
+  picture: {
+    type: String,
+  },
+
+  fullName: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female", "other"],
+    required: true,
+  },
+});
+
+const UserModel: Model<IUser> = mongoose.model<IUser>("User", UserSchema);
+
+export default UserModel;
