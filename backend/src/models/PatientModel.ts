aa5b294bd2ccf81d938fabd021d263c26c890347
MoeShoumar <mks38@mail.aub.edu>
2023-04-15 23:52:21 +0300

    added patient model

---
+++ backend/src/models/PatientModel.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Model } from "mongoose";
+
+export interface IPatient extends mongoose.Document {
+  user: string;
+  consultations: {
+    doctor: string;
+    date: Date;
+  }[];
+}
+
+const Schema = mongoose.Schema;
+const PatientSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  consultations: [
+    {
+      doctor: {
+        type: Schema.Types.ObjectId,
+        ref: "Doctor",
+        required: true,
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Patient = mongoose.model<IPatient>("Patient", PatientSchema);
+
+export default Patient;
