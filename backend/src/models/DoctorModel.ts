ebccd2bff1abafb903f807ae984235bedcf4a5e2
MoeShoumar <mks38@mail.aub.edu>
2023-04-16 09:36:32 +0300

    edited doctor model

--- backend/src/models/DoctorModel.ts
+++ backend/src/models/DoctorModel.ts
@@ -2,15 +2,13 @@ import mongoose, { Document, Model } from "mongoose";
 
 export interface IDoctor extends mongoose.Document {
   user: string;
-  medicalHistory: string;
-  vitals: string;
   specialization: string;
-  price: number;
-  workingHours: {
+  price?: number;
+  workingHours?: {
     start: string;
     end: string;
   };
-  consultations: {
+  consultations?: {
     doctor: string;
     date: Date;
   }[];
@@ -24,20 +22,16 @@ const DoctorSchema = new Schema({
   },
   specialization: {
     type: String,
-    required: true,
   },
   price: {
     type: Number,
-    required: true,
   },
   workingHours: {
     start: {
       type: String,
-      required: true,
     },
     end: {
       type: String,
-      required: true,
     },
   },
   patients: [
