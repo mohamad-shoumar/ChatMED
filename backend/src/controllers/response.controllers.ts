a08602f184301b79f870bd55a176348f8286008f
MoeShoumar <mks38@mail.aub.edu>
2023-04-17 23:50:10 +0300

    created openai api

--- backend/src/controllers/response.controllers.ts
+++ backend/src/controllers/response.controllers.ts
@@ -1,23 +1,47 @@
 // responseByChat
 import { Request, Response } from "express";
-import Joi from "joi";
-import bcrypt from "bcrypt";
-import User, { IUser } from "../models/UserModel";
-import jwt from "jsonwebtoken";
-import Doctor, { IDoctor } from "../models/DoctorModel";
-import Patient, { IPatient } from "../models/PatientModel";
+import { Configuration, OpenAIApi } from "openai";
+import Patient from "../models/PatientModel";
+import MedicalHistory from "../models/MedicalHistoryModel";
+import ResponseModel from "../models/ResponseModel";
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-export const responseByChat = async (req: Request, response: Response) => {
+export const responseByChat = async (req: Request, res: Response) => {
   try {
-    // const { prompt } = req.body;
-    // const response = await OpenAI.("completion", {
-    //     model: "text-davinci-003",
-    //     prompt: (symptoms,medicalhistory),
-    //     tempreture: 0.9,
-    //     max_tokens: 1000,
-    //     n:1
-    // });
+    const patientId = req.body.user.id;
+    const doctorId = req.body.doctor;
+    const retrievedMedicalhistory = await MedicalHistory.findOne({
+      user: patientId,
+    });
+    const symptoms = req.body.symptoms;
+    console.log(doctorId);
+
+    const response: any = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(symptoms, retrievedMedicalhistory),
+      temperature: 0.7,
+      max_tokens: 1000,
+      n: 1,
+    });
+    const responseData = JSON.parse(response.data.choices[0].text.trim());
+    const responseModel = new ResponseModel({
+      patient: patientId,
+      doctor: doctorId,
+      symptoms: symptoms,
+      diagnosis: responseData.diagnosis,
+      treatmentPlan: responseData.treatmentPlan,
+      status: "pending",
+    });
+
+    await responseModel.save();
+    return res.status(200).json({
+      message: "success",
+      data: responseData,
+    });
   } catch (error: any) {
-    response.status(500).json({ message: error });
+    console.log(error);
   }
 };
