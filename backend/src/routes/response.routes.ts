4d4c564a94dc1962d178b83ee22ec1f201966c5d
MoeShoumar <mks38@mail.aub.edu>
2023-04-17 23:52:39 +0300

    created response route

---
+++ backend/src/routes/response.routes.ts
@@ -0,0 +1,12 @@
+// post choose doctor and enter sympotoms (response model)
+// get response by doctor
+import { Router } from "express";
+const router: Router = Router();
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { patientMiddleware } from "../middlewares/patient.middleware";
+import { doctorMiddleware } from "../middlewares/doctor.middleware";
+import { responseByChat } from "../controllers/response.controllers";
+
+router.post("/", authMiddleware, responseByChat);
+
+export default router;
