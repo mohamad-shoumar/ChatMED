5a779a30b688bca192654795a6ce3a2488d54c04
MoeShoumar <mks38@mail.aub.edu>
2023-04-17 23:54:26 +0300

    added navbar styling

---
+++ frontend/src/pages/MedicalHistory/MedicalHistory.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar";
+
+const MedicalHistory = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default MedicalHistory;
