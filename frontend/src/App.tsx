22dadc016c11b8f9e84bec2ccb3f1386961c2802
MoeShoumar <mks38@mail.aub.edu>
2023-04-17 23:53:14 +0300

    installed scss depencies and set up app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import logo from "./logo.svg";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Authentication from "./pages/Authentication/Authentication";
+import MedicalHistory from "./pages/MedicalHistory/MedicalHistory";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/authentication" element={<Authentication />} />
+        <Route path="/medical-history" element={<MedicalHistory />} />
       </Routes>
     </BrowserRouter>
   );
