f4d6740af8ea82c1d0099bb08480c5cd0f766235
MoeShoumar <mks38@mail.aub.edu>
2023-04-18 13:15:44 +0300

    changed css to sccs, fixed the jsx in medical history page component

--- frontend/src/pages/MedicalHistory/MedicalHistory.tsx
+++ frontend/src/pages/MedicalHistory/MedicalHistory.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import styles from "../../styles/MedicalHistory/MedicalHistory.module.scss";
 import NavBar from "../../components/NavBar/NavBar";
 import SideNavBar from "../../components/SideNavBar/SideNavBar";
+import { NumericFormat } from "react-number-format";
 import DatePicker from "react-datepicker";
+import "react-datetime-picker/dist/DateTimePicker.css";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Register from "../../components/Register/Register";
 import LogIn from "../../components/LogIn/LogIn";
 
 const MedicalHistory = () => {
-  const [dateOfBirth, setDateOfBirth] = useState(null);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const navigate = useNavigate();
 
   return (
     <div>
@@ -30,17 +34,33 @@ const MedicalHistory = () => {
           <div className={styles.medicalHistoryForm}>
             <div className={styles.height}>
               <h3>Height</h3>
-              <input type="text" placeholder="Height" />
+              <NumericFormat
+                placeholder="Height"
+                allowNegative={false}
+                decimalScale={2}
+                suffix="cm"
+                customInput={(props: any) => <input {...props} type="text" />}
+              />
             </div>
             <div className={styles.weight}>
               <h3>Weight</h3>
-              <input type="text" placeholder="Weight" />
+              <NumericFormat
+                placeholder="Weight"
+                allowNegative={false}
+                decimalScale={2}
+                suffix="kg"
+                customInput={(props: any) => <input {...props} type="text" />}
+              />
             </div>
             <div className={styles.dob}>
               <h3>Date of Birth</h3>
               <DatePicker
-                selected={dateOfBirth}
-                onChange={(date: any) => setDateOfBirth(date)}
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                dateFormat="dd/MM/yyyy"
+                showYearDropdown
+                scrollableYearDropdown
+                isClearable
               />
             </div>
           </div>
