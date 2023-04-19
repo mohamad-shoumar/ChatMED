49c1d23c783d92ed2c607457dd0d34f3129068da
MoeShoumar <mks38@mail.aub.edu>
2023-04-19 16:05:42 +0300

    finalized sidebar with navigation functionality

--- frontend/src/components/SideNavBar/SideNavBar.tsx
+++ frontend/src/components/SideNavBar/SideNavBar.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from "../../styles/SideNavBar/SideNavBar.module.scss";
 import Union from "../../assets/sidebar/Union.png";
-import Vector from "../../assets/sidebar/Vector.png";
 import history from "../../assets/sidebar/history.png";
 import vitals from "../../assets/sidebar/vitals.png";
-import Icon from "../../assets/sidebar/Icon.png";
+import dash from "../../assets/sidebar/dash.png";
 import chats from "../../assets/sidebar/chats.png";
 import logs from "../../assets/sidebar/logs.png";
 import sun from "../../assets/sidebar/sun.png";
+import Icon from "../../assets/sidebar/Icon.png";
+import { useNavigate } from "react-router-dom";
 
 const SideNavBar = () => {
+  const navigate = useNavigate();
+
   return (
     <div>
       <div className={styles.main}>
@@ -18,38 +21,50 @@ const SideNavBar = () => {
         </div>
         <div className={styles.submain}>
           <div className={styles.dash}>
-            <img src={Vector} alt="logo" />
+            <img src={dash} alt="logo" />
             <div className={styles.Dashboard}>Dashboard</div>
           </div>
-          <div className={styles.link}>
+          <div
+            className={`${styles.link} ${styles.hover}`}
+            onClick={() => navigate("/medicalhistory")}
+          >
             <div className={styles.linkleft}>
               <img src={history} alt="logo" />
               <div className={styles.word}>Medical Hsitory</div>
             </div>
             <img src={Icon} alt="arrow" />
           </div>
-          <div className={styles.link}>
+          <div
+            className={`${styles.link} ${styles.hover}`}
+            onClick={() => navigate("/authentication")}
+          >
             <div className={styles.linkleft}>
               <img src={vitals} alt="logo" />
               <div className={styles.word}> Vitals</div>
             </div>
             <img src={Icon} alt="arrow" />
           </div>
-          <div className={styles.link}>
+          <div
+            className={`${styles.link} ${styles.hover}`}
+            onClick={() => navigate("")}
+          >
             <div className={styles.linkleft}>
               <img src={chats} alt="logo" />
               <div className={styles.word}>Chats</div>
             </div>
             <img src={Icon} alt="arrow" />
           </div>
-          <div className={styles.link}>
+          <div
+            className={`${styles.link} ${styles.hover}`}
+            onClick={() => navigate("")}
+          >
             <div className={styles.linkleft}>
               <img src={logs} alt="logo" />
               <div className={styles.word}>Log Out</div>
             </div>
             <img src={Icon} alt="arrow" />
           </div>
-          <div className={styles.darkmode}>
+          <div className={`${styles.darkmode} ${styles.hover}`}>
             <div className={styles.linkleft}>
               <img src={sun} alt="logo" />
               <div className={styles.word}>Dark Mode</div>
