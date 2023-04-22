0334bfb005c46b9866be3b5c0287707ea8ce2898
MoeShoumar <mks38@mail.aub.edu>
2023-04-22 23:52:35 +0300

    created chatGPT analysis route

---
+++ backend/src/routes/analyze.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+const router: Router = Router();
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { patientMiddleware } from "../middlewares/patient.middleware";
+import { doctorMiddleware } from "../middlewares/doctor.middleware";
+import { analyzeByChat } from "../controllers/analyze.controllers";
+
+router.post("/", authMiddleware, analyzeByChat);
+
+export default router;
