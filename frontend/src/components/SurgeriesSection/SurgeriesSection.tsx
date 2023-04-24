75ff58261b54a8c7ef01d247f56e6531fbb75c6d
MoeShoumar <mks38@mail.aub.edu>
2023-04-24 15:58:00 +0300

    <frontend> mapped through the surgies added in the SurgeriesSection component

--- frontend/src/components/SurgeriesSection/SurgeriesSection.tsx
+++ frontend/src/components/SurgeriesSection/SurgeriesSection.tsx
@@ -137,6 +137,28 @@ const SurgeriesSection = ({ onUpdateSurgeries }: surgeryProps) => {
           </Box>
         </Box>
       )}
+      {surgeries.map((surgery, index) => (
+        <Box
+          key={index}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+            marginBottom: "8px",
+          }}
+        >
+          <Box sx={{ width: "40%" }}>
+            <Typography variant="body1">{surgery.name}</Typography>
+          </Box>
+          <Box sx={{ width: "40%" }}>
+            <Typography variant="body1">{surgery.date}</Typography>
+          </Box>
+          <IconButton onClick={() => handleDeleteSurgery(index)} size="small">
+            <DeleteIcon />
+          </IconButton>
+        </Box>
+      ))}
     </Box>
   );
 };
