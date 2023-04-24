c9e74ac3fa6a85531eaffadcd986f751121804e9
MoeShoumar <mks38@mail.aub.edu>
2023-04-24 23:22:02 +0300

    added delete condition jsx section for conditions component

--- frontend/src/components/ConditionsSection/ConditionsSection.tsx
+++ frontend/src/components/ConditionsSection/ConditionsSection.tsx
@@ -1,50 +1,49 @@
 import React, { useState, useEffect } from "react";
 import {
-Grid,
-Typography,
-IconButton,
-TextField,
-FormControl,
-InputLabel,
-Select,
-MenuItem,
-Button,
-Box,
-Divider,
-Paper,
+  Grid,
+  Typography,
+  IconButton,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Box,
+  Divider,
+  Paper,
 } from "@mui/material";
 import {
-Delete as DeleteIcon,
-Clear as ClearIcon,
-Check as CheckIcon,
+  Delete as DeleteIcon,
+  Clear as ClearIcon,
+  Check as CheckIcon,
 } from "@mui/icons-material";
 
 interface conditionProps {
-onUpdateConditions: (conditions: { name: string; date: number }[]) => void;
+  onUpdateConditions: (conditions: { name: string; date: number }[]) => void;
 }
 
-
 const ConditionsSection = ({ onUpdateConditions }: conditionProps) => {
-const [conditionName, setConditionName] = useState("");
-const [date, setDate] = useState(0);
-const [conditions, setConditions] = useState<
-{ name: string; date: number }[]
->([]);
-const [showInputs, setShowInputs] = useState(false);
-const [activeButton, setActiveButton] = useState(false);
-const handleAddCondition = () => {
+  const [conditionName, setConditionName] = useState("");
+  const [date, setDate] = useState(0);
+  const [conditions, setConditions] = useState<
+    { name: string; date: number }[]
+  >([]);
+  const [showInputs, setShowInputs] = useState(false);
+  const [activeButton, setActiveButton] = useState(false);
+  const handleAddCondition = () => {
     const newCondition = { name: conditionName, date: date };
     const updatedConditions = [...conditions, newCondition];
     setConditions(updatedConditions);
     onUpdateConditions(updatedConditions);
     setDate(0);
-    };
-    const handleDeleteCondition = (index: any) => {
+  };
+  const handleDeleteCondition = (index: any) => {
     const updatedConditions = [...conditions];
     updatedConditions.splice(index, 1);
     setConditions(updatedConditions);
-    };
-return (
+  };
+  return (
     <Box
       sx={{
         display: "flex",
@@ -74,45 +73,45 @@ return (
             setActiveButton(true);
           }}
         >
-             <ClearIcon />
-      </IconButton>
-      <IconButton
-        sx={{
-          "&:hover": {
-            color: activeButton ? "green" : "initial",
-          },
-          "&:focus": {
-            outline: activeButton ? "2px solid green" : "none",
-            outlineOffset: 2,
-            color: activeButton ? "green" : "initial",
-          },
-        }}
-        onClick={() => {
-          setShowInputs(true);
-          setActiveButton(true);
-        }}
-        onChange={() => {
-          setActiveButton(true);
-        }}
-      >
-        <CheckIcon />
-      </IconButton>
-    </Box>
-    {showInputs && (
-      <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-        <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
-          <Box sx={{ flex: 1 }}>
-            <InputLabel id="Condition-label">Condition</InputLabel>
-            <TextField
-              id="Condition-label"
-              fullWidth
-              label="Condition Name"
-              variant="outlined"
-              value={conditionName}
-              onChange={(event) => setConditionName(event.target.value)}
-            />
-          </Box>
-          <Box sx={{ flex: 1 }}>
+          <ClearIcon />
+        </IconButton>
+        <IconButton
+          sx={{
+            "&:hover": {
+              color: activeButton ? "green" : "initial",
+            },
+            "&:focus": {
+              outline: activeButton ? "2px solid green" : "none",
+              outlineOffset: 2,
+              color: activeButton ? "green" : "initial",
+            },
+          }}
+          onClick={() => {
+            setShowInputs(true);
+            setActiveButton(true);
+          }}
+          onChange={() => {
+            setActiveButton(true);
+          }}
+        >
+          <CheckIcon />
+        </IconButton>
+      </Box>
+      {showInputs && (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+          <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+            <Box sx={{ flex: 1 }}>
+              <InputLabel id="Condition-label">Condition</InputLabel>
+              <TextField
+                id="Condition-label"
+                fullWidth
+                label="Condition Name"
+                variant="outlined"
+                value={conditionName}
+                onChange={(event) => setConditionName(event.target.value)}
+              />
+            </Box>
+            <Box sx={{ flex: 1 }}>
               <InputLabel id="date-label">Onset-Date</InputLabel>
               <TextField
                 fullWidth
@@ -124,7 +123,7 @@ return (
             </Box>
           </Box>
           <Box>
-          <Button
+            <Button
               variant="contained"
               color="primary"
               onClick={handleAddCondition}
@@ -134,4 +133,28 @@ return (
           </Box>
         </Box>
       )}
-      
+      {conditions.map((condition, index) => (
+        <Box
+          key={index}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+            marginBottom: "8px",
+          }}
+        >
+          <Box sx={{ width: "40%" }}>
+            <Typography variant="body1">{condition.name}</Typography>
+          </Box>
+          <Box sx={{ width: "40%" }}>
+            <Typography variant="body1">{condition.date}</Typography>
+          </Box>
+          <IconButton onClick={() => handleDeleteCondition(index)} size="small">
+            <DeleteIcon />
+          </IconButton>
+        </Box>
+      ))}
+    </Box>
+  );
+};
