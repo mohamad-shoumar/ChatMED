623396638e9af3eb1decf0ea8039921d1d076479
MoeShoumar <mks38@mail.aub.edu>
2023-04-28 23:35:56 +0300

    fixed get blood pressure api

--- backend/src/controllers/vitals.controller.ts
+++ backend/src/controllers/vitals.controller.ts
@@ -10,22 +10,49 @@ export const addBloodPressure = async (req: Request, res: Response) => {
   try {
     const value = req.body.value;
     const userId = req.body.user.id;
+    if (!value || !userId) {
+      return res.status(400).json({ message: "Invalid input data" });
+    }
+    let vitals = await Vitals.findOne({ user: userId });
 
-    let vital = await Vitals.findOne({ user: userId });
-
-    if (!vital) {
-      vital = new Vitals({ user: userId, bloodPressure: [{ value }] });
+    if (!vitals) {
+      vitals = new Vitals({ user: userId });
     } else {
-      if (!vital.bloodPressure) {
-        vital.bloodPressure = [{ value }];
-        console.log(vital.bloodPressure);
+      if (!vitals.bloodPressure) {
+        vitals.bloodPressure = [{ value }];
       } else {
-        vital.bloodPressure.push({ value });
+        vitals.bloodPressure.push({ value });
       }
     }
-    await vital.save();
+    await vitals.save();
 
-    res.json(vital);
+    res.json(vitals);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+// add sugar api
+export const addSugar = async (req: Request, res: Response) => {
+  try {
+    const value = req.body.value;
+    const userId = req.body.user.id;
+    if (!value || !userId) {
+      return res.status(400).json({ message: "Invalid input data" });
+    }
+
+    let vitals = await Vitals.findOne({ user: userId });
+    if (!vitals) {
+      vitals = new Vitals({ user: userId });
+    } else {
+      if (!vitals.bloodsugar) {
+        vitals.bloodsugar = [{ value }];
+      } else {
+        vitals.bloodsugar.push({ value });
+      }
+    }
+    await vitals.save();
+    res.json(vitals);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
@@ -43,38 +70,13 @@ export const getBloodPressure = async (req: Request, res: Response) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.json(retrievedBloodPressure.bloodPressure);
+    res.json(retrievedBloodPressure);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
   }
 };
-// add sugar api
-export const addSugar = async (req: Request, res: Response) => {
-  try {
-    const value = req.body.value;
-    const userId = req.body.user.id;
-    if (!value || !userId) {
-      return res.status(400).json({ message: "Invalid input data" });
-    }
 
-    let vitals = await Vitals.findOne({ user: userId });
-    if (!vitals) {
-      vitals = await new Vitals({ user: userId });
-    } else {
-      if (!vitals.bloodsugar) {
-        vitals.bloodsugar = [{ value }];
-      } else {
-        vitals.bloodsugar.push({ value });
-      }
-    }
-    await vitals.save();
-    res.json(vitals);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server error" });
-  }
-};
 // get sugar api
 export const getSugar = async (req: Request, res: Response) => {
   try {
