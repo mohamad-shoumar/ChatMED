71386884c9186fe15511b09b32243d514c7684d0
MoeShoumar <mks38@mail.aub.edu>
2023-04-28 23:39:12 +0300

    Added BarGraph function

--- frontend/src/components/BarGraph/BarGraph.tsx
+++ frontend/src/components/BarGraph/BarGraph.tsx
@@ -15,4 +15,81 @@ export default function BloodPressure() {
     );
     const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
     const token = localStorage.getItem("token");
+    const getBloodPressure = async () => {
+      try {
+        const response = await API.getAPI(
+          `${base_url}vitals/bloodpressure`,
+          token!
+        );
+        console.log(response.bloodPressure);
+        const systolicValues = response.bloodPressure.map((item: any) => {
+          const [systolic, diastolic] = item.value.split("/");
+          return systolic;
+        });
 
+        const diastolicValues = response.bloodPressure.map((item: any) => {
+          const [systolic, diastolic] = item.value.split("/");
+          return diastolic;
+        });
+
+        const data = {
+          labels: response.bloodPressure.map((item: any) =>
+            item.timestamp.substring(0, 10)
+          ),
+
+          datasets: [
+            {
+              label: " Systolic Blood Pressure",
+              data: systolicValues,
+              fill: false,
+              tension: 0.4,
+              borderColor: documentStyle.getPropertyValue("--blue-500"),
+            },
+            {
+              label: "Daistolic Blood Pressure",
+              data: diastolicValues,
+              fill: false,
+              borderDash: [5, 5],
+              tension: 0.4,
+              borderColor: documentStyle.getPropertyValue("--teal-500"),
+            },
+          ],
+        };
+        const options = {
+          maintainAspectRatio: false,
+          aspectRatio: 2,
+          plugins: {
+            legend: {
+              labels: {
+                color: textColor,
+              },
+            },
+          },
+          scales: {
+            x: {
+              ticks: {
+                color: textColorSecondary,
+              },
+              grid: {
+                color: surfaceBorder,
+              },
+            },
+            y: {
+              ticks: {
+                color: textColorSecondary,
+              },
+              grid: {
+                color: surfaceBorder,
+              },
+            },
+          },
+        };
+        setChartData(data);
+        setChartOptions(options);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBloodPressure();
+  }, []);
+}
