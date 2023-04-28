5a89f8eda0e4b2db6b02f10cb39dcf8ffbc0a753
MoeShoumar <mks38@mail.aub.edu>
2023-04-28 23:40:19 +0300

    Imported LineGraph adn created UseEffect

---
+++ frontend/src/components/BloodSugarGraph/BloodSugarGraph.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
+import { API } from "../../API/API";
+import { base_url } from "../../API/API";
+
+export default function BloodSugarGraph() {
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue("--text-color");
+    const textColorSecondary = documentStyle.getPropertyValue(
+      "--text-color-secondary"
+    );
+    const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
+    const token = localStorage.getItem("token");
+
+    
+}
