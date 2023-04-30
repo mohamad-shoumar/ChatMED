4a059d1c9de5c8ffbdbdfce0e28cf2d7c908bc07
MoeShoumar <mks38@mail.aub.edu>
2023-04-30 22:29:58 +0300

    Split routes into patient and doctor, also Created routes for doctor dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,17 +7,28 @@ import PatientProfile from "./pages/PatientProfile/PatientProfile";
 import PatientDashboard from "./pages/PatientDashboard/PatientDashboard";
 import Vitals from "./pages/Vitals/Vitals";
 import Consultation from "./pages/Consultation/Consultation";
+import Chats from "./pages/Chats/Chats";
+import DoctorDashboard from "./pages/DoctorPages/DoctorDashboard/DoctorDashboard";
+import "primereact/resources/primereact.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/authentication" element={<Authentication />} />
-        <Route path="/medicalhistory" element={<MedicalHistory />} />
-        <Route path="/profile" element={<PatientProfile />} />
-        <Route path="/dashboard" element={<PatientDashboard />} />
-        <Route path="/vitals" element={<Vitals />} />
-        <Route path="/consultation" element={<Consultation />} />
+        <Route path="/patient/*">
+          <Route path="authentication" element={<Authentication />} />
+          <Route path="medicalhistory" element={<MedicalHistory />} />
+          <Route path="profile" element={<PatientProfile />} />
+          <Route path="dashboard" element={<PatientDashboard />} />
+          <Route path="vitals" element={<Vitals />} />
+          <Route path="consultation" element={<Consultation />} />
+          <Route path="chat" element={<Chats />} />
+        </Route>
+        <Route path="/doctor/*">
+          <Route path="dashboard" element={<DoctorDashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
