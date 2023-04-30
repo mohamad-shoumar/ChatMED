d12d5163e6ab7d820e8de9bcaccd18ce07df1ab7
MoeShoumar <mks38@mail.aub.edu>
2023-04-30 22:30:19 +0300

    changed typography settings

--- frontend/src/FireBase.tsx
+++ frontend/src/FireBase.tsx
@@ -1,13 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-const firebaseConfig = {
-  apiKey: "AIzaSyCElEY3b7uV7JQKVvXKEQOcyaw5PF3zrpU",
-  authDomain: "image-upload-c464d.firebaseapp.com",
-  projectId: "image-upload-c464d",
-  storageBucket: "image-upload-c464d.appspot.com",
-  messagingSenderId: "1006273285960",
-  appId: "1:1006273285960:web:ad7d5d1628dba37d662d7d",
-  measurementId: "G-XH0YZQ4HTE",
-};
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+// import { initializeApp } from "firebase/app";
+// import { getStorage } from "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCElEY3b7uV7JQKVvXKEQOcyaw5PF3zrpU",
+//   authDomain: "image-upload-c464d.firebaseapp.com",
+//   projectId: "image-upload-c464d",
+//   storageBucket: "image-upload-c464d.appspot.com",
+//   messagingSenderId: "1006273285960",
+//   appId: "1:1006273285960:web:ad7d5d1628dba37d662d7d",
+//   measurementId: "G-XH0YZQ4HTE",
+// };
+// const app = initializeApp(firebaseConfig);
+// export const storage = getStorage(app);
+export {};
