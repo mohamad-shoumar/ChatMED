956bf1e1aebc2be84c4159bba79c476d622fac73
MoeShoumar <mks38@mail.aub.edu>
2023-05-02 23:22:09 +0300

    <frontend> Clean code

--- frontend/src/pages/Vitals/Vitals.tsx
+++ frontend/src/pages/Vitals/Vitals.tsx
@@ -30,81 +30,16 @@ const Vitals = () => {
           <div className={styles.headerContainer}>
             <h2>Vitals</h2>
             <div className={styles.headerButtons}>
-              <Button label="Submit" size="small" />
+              <Button
+                severity="secondary"
+                style={{ marginRight: "2rem" }}
+                label="Submit"
+                size="small"
+              />
             </div>
           </div>
           <div className={styles.mainContent}>
-            {/* <div className={styles.inputsContainer}>
-              <div className={styles.pressureInput}>
-                <div className={styles.pressureVertical}>
-                  <h6>Enter BloodPressure</h6>
-                  <div className={styles.pressureHorizantal}>
-                    <div>
-                      <label htmlFor="mmHg" className="font-bold block mb-2">
-                        Systolic Pressure
-                      </label>
-                      <InputNumber
-                        inputId="mmHg"
-                        value={sys}
-                        onValueChange={(e: InputNumberValueChangeEvent) =>
-                          setSys(e.value)
-                        }
-                        suffix=" mmHg"
-                      />
-                    </div>
-                    <div>
-                      <label htmlFor="mmHg" className="font-bold block mb-2">
-                        Daistolic Pressure
-                      </label>
-                      <InputNumber
-                        inputId="mmHg"
-                        value={dais}
-                        onValueChange={(e: InputNumberValueChangeEvent) =>
-                          setDais(e.value)
-                        }
-                        suffix=" mmHg"
-                      />
-                    </div>
-                    <Button
-                      label="Submit"
-                      size="small"
-                      className="mt-5 mb-4 "
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className={styles.pressureInput}>
-                <div className={styles.pressureVertical}>
-                  <h6>Enter Blood Glucose</h6>
-                  <div className={styles.pressureHorizantal}>
-                    <div>
-                      <label htmlFor="mg/dL" className="font-bold block mb-2 ">
-                        Glucose Level
-                      </label>
-                      <InputNumber
-                        inputId="mg/dL"
-                        value={sugar}
-                        onValueChange={(e: InputNumberValueChangeEvent) =>
-                          setSugar(e.value)
-                        }
-                        suffix=" mg/dL"
-                      />
-                    </div>
-                    <Button
-                      label="Submit"
-                      size="small"
-                      className="mt-5 mb-4 "
-                    />
-                  </div>
-                </div>
-              </div>
-            </div> */}
-            {/* <div className={styles.logsContainer}>
-              <div className={styles.sugarLogs}> */}
             <BloodSugarTable />
-            {/* </div>
-              <div className={styles.pressureLogs}></div>
-            </div> */}
           </div>
         </div>
       </div>
