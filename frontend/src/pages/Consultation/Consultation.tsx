1995cb4cefbf9104d0fd123b922ce76dd54d9888
MoeShoumar <mks38@mail.aub.edu>
2023-05-02 23:19:03 +0300

    imported the components to the doctors page and fetched the data from the backend

--- frontend/src/pages/Consultation/Consultation.tsx
+++ frontend/src/pages/Consultation/Consultation.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 import styles from "../../styles/Consultation/Consultation.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SideNavBar from "../../components/SideNavBar/SideNavBar";
 import NavBar from "../../components/NavBar/NavBar";
-import DoctorCard from "../../components/Card/Card";
 import "../../Prime-theme/theme.css";
 import { base_url } from "../../API/API";
 import { API } from "../../../src/API/API";
-
+import Search from "../../components/Search/Search";
+import SortDoctor from "../../components/sortDoctor/sortDoctor";
+import { Dialog } from "@mui/material";
+import DoctorCard from "../../components/DoctorCard/DoctorCard";
+interface Doctor {
+  id: number;
+  name: string;
+  price: number;
+  specialty: string;
+}
 const Consultation = () => {
+  const [doctors, setDoctors] = useState<Doctor[]>([]);
+  const [specialtyFilter, setSpecialtyFilter] = useState<string>("all");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    API.getAPI(`${base_url}patient/getdoctors`, token!).then((response) => {
+      setDoctors(response);
+      console.log(response);
+    });
+  }, []);
+  const handleSpecialtyChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSpecialtyFilter(event.target.value);
+  };
+  const filteredDoctors =
+    specialtyFilter === "all"
+      ? doctors
+      : doctors.filter((doctor) => doctor.specialty === specialtyFilter);
   return (
     <div>
       <NavBar />
@@ -19,31 +46,34 @@ const Consultation = () => {
             <div className={styles.medicalHistoryTitle}>
               <h2>Consultations</h2>
             </div>
-            <div className={styles.submitbtn}>search</div>
+            <div className={styles.submitbtn}>
+              <Search />
+            </div>
           </div>
           <div className={styles.medicalHistoryForm}>
-            <div className={styles.top}>
-              <DoctorCard />
-            </div>
             <div className={styles.bot}>
-              <div className={styles.botRight}>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
-                minus sapiente modi debitis rem maiores nemo qui corporis unde
-                delectus eius, officia at suscipit quos voluptatum, expedita
-                facilis explicabo ipsam.
-              </div>
               <div className={styles.botLeft}>
+                <SortDoctor onChange={handleSpecialtyChange} />
+                <div className={styles.docCards}>
+                  {filteredDoctors.map((doctor) => (
+                    <DoctorCard
+                      key={doctor.id}
+                      doctor={{
+                        name: "",
+                        avatar: "",
+                        price: 0,
+                        specialty: "",
+                      }}
+                    />
+                  ))}
+                </div>
+              </div>
+              <div className={styles.botRight}>
                 <div className={styles.symptoms}>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Itaque minus sapiente modi debitis rem maiores nemo qui
-                  corporis unde delectus eius, officia at suscipit quos
-                  voluptatum, expedita facilis explicabo ipsam.
+                  <h3>Symptoms</h3>
                 </div>
                 <div className={styles.response}>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Itaque minus sapiente modi debitis rem maiores nemo qui
-                  corporis unde delectus eius, officia at suscipit quos
-                  voluptatum, expedita facilis explicabo ipsam.
+                  <Dialog open={false} />
                 </div>
               </div>
             </div>
