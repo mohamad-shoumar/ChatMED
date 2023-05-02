3e0c680332c3868c43f9f797edfe7e8b2a07c613
MoeShoumar <mks38@mail.aub.edu>
2023-05-02 23:11:44 +0300

    added .env to git ignore

--- backend/src/routes/upload.routes.tsx
+++ backend/src/routes/upload.routes.tsx
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import mutler from "multer";
 const router: Router = Router();
-import { uploadFile } from "../controllers/patient.controllers";
+import { uploadFile, getFile } from "../controllers/patient.controllers";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
-router.post("/upload", mutler, uploadFile);
+router.post("/upload", authMiddleware, uploadFile);
+router.get("/upload", authMiddleware, getFile);
 
 export default router;
