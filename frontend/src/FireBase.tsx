c0abf0f576d1caa56f9243019135570cba3d5b57
MoeShoumar <mks38@mail.aub.edu>
2023-05-02 23:12:19 +0300

    Reconfigured firebase

--- frontend/src/FireBase.tsx
+++ frontend/src/FireBase.tsx
@@ -1,15 +1,14 @@
-// import { initializeApp } from "firebase/app";
-// import { getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCElEY3b7uV7JQKVvXKEQOcyaw5PF3zrpU",
-//   authDomain: "image-upload-c464d.firebaseapp.com",
-//   projectId: "image-upload-c464d",
-//   storageBucket: "image-upload-c464d.appspot.com",
-//   messagingSenderId: "1006273285960",
-//   appId: "1:1006273285960:web:ad7d5d1628dba37d662d7d",
-//   measurementId: "G-XH0YZQ4HTE",
-// };
-// const app = initializeApp(firebaseConfig);
-// export const storage = getStorage(app);
-export {};
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "image-upload-3bfe6.firebaseapp.com",
+  projectId: "image-upload-3bfe6",
+  storageBucket: "image-upload-3bfe6.appspot.com",
+  messagingSenderId: "17998004745",
+  appId: "1:17998004745:web:db678e24a9dca82498aed8",
+  measurementId: "G-S457FQ67EV",
+};
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
