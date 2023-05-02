7d1441570eb6e5273bf70f7e7675ef1877eb481a
MoeShoumar <mks38@mail.aub.edu>
2023-05-02 23:12:04 +0300

    Splitted routes in the app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,26 +8,29 @@ import PatientDashboard from "./pages/PatientDashboard/PatientDashboard";
 import Vitals from "./pages/Vitals/Vitals";
 import Consultation from "./pages/Consultation/Consultation";
 import Chats from "./pages/Chats/Chats";
-import DoctorDashboard from "./pages/DoctorPages/DoctorDashboard/DoctorDashboard";
+
 import "primereact/resources/primereact.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
+import DoctorDashboard from "./pages/DoctorPages/DoctorDashboard/DoctorDashboard";
+import DoctorProfile from "./pages/DoctorPages/DoctorProfile/DoctorProfile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="authentication" element={<Authentication />} />
         <Route path="/patient/*">
-          <Route path="authentication" element={<Authentication />} />
           <Route path="medicalhistory" element={<MedicalHistory />} />
           <Route path="profile" element={<PatientProfile />} />
           <Route path="dashboard" element={<PatientDashboard />} />
           <Route path="vitals" element={<Vitals />} />
           <Route path="consultation" element={<Consultation />} />
-          <Route path="chat" element={<Chats />} />
         </Route>
+        <Route path="chat" element={<Chats />} />
         <Route path="/doctor/*">
           <Route path="dashboard" element={<DoctorDashboard />} />
+          <Route path="profile" element={<DoctorProfile />} />
         </Route>
       </Routes>
     </BrowserRouter>
