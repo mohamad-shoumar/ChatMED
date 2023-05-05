1e491d574af3e8aa8a1e455e0fb7b994b5cfe942
MoeShoumar <mks38@mail.aub.edu>
2023-05-05 21:37:04 +0300

    <frontend>created sidebar component

--- frontend/src/components/Chat/SideBar.tsx
+++ frontend/src/components/Chat/SideBar.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import styles from "../../styles/ChatPage/ChatPage.module.scss";
 import Search from "./Search";
 import Chats from "./Chats";
+import NavBar from "./NavBar";
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebar}>
+      <NavBar />
       <Search />
       <Chats />
     </div>
