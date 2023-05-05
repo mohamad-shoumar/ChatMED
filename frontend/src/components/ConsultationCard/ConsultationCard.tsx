dc626557aff5907e28b1993134e9570162c97e33
MoeShoumar <mks38@mail.aub.edu>
2023-05-05 21:37:32 +0300

    <frontend>edited consulation crad styling

--- frontend/src/components/ConsultationCard/ConsultationCard.tsx
+++ frontend/src/components/ConsultationCard/ConsultationCard.tsx
@@ -9,25 +9,26 @@ export default function ConsultationCard() {
       className="card flex flex-direction-column justify-content-center align-items-center"
       style={{
         background: "secondary",
-
         padding: "10px",
       }}
     >
       <div
         style={{
           background: "white",
-          width: "90%",
+          minWidth: "95%",
           padding: "10px",
           minHeight: "60px",
           display: "flex",
           flexDirection: "column",
           justifyContent: "flex-end",
+          flexGrow: 1,
+
+          boxShadow: "0px 0px 7px 0px rgba(0,0,0,0.75)",
         }}
       >
-        <p style={{ fontSize: "14px", minHeight: "60px" }} className="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-          esse, cupiditate neque quas! Lorem ipsum dolor, sit amet consectetur
-          i, nostrum animi?
+        <p style={{ fontSize: "14px", minHeight: "30px" }} className="m-0">
+          Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet
+          isquam? Dolores, totam assumenda. adipisicing elit. Inventore
         </p>
         <div style={{ fontSize: "10px" }}>28/4/2023</div>
       </div>
