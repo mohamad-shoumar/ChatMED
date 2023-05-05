e128869d765e7c287ff7cf017962a5ae15111414
MoeShoumar <mks38@mail.aub.edu>
2023-05-05 20:55:20 +0300

    <frontend>added prop label to button component

--- frontend/src/components/Button2/Button2.tsx
+++ frontend/src/components/Button2/Button2.tsx
@@ -1,13 +1,14 @@
 import { Button } from "primereact/button";
 interface Button2Props {
-  onClick?: (e: any) => Promise<void>;
+  onClick?: (e: any) => void;
+  label?: string;
 }
-const Button2 = ({ onClick }: Button2Props) => {
+const Button2 = ({ onClick, label }: Button2Props) => {
   return (
     <div>
       <Button
         severity="secondary"
-        label="Submit"
+        label={label || "Submit"}
         size="small"
         onClick={onClick}
       />
