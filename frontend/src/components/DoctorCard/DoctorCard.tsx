72711ab17ead82c7bc6a219c5bf2c93ec366b52c
MoeShoumar <mks38@mail.aub.edu>
2023-05-05 21:39:57 +0300

    <frontend>fixed doctor card

--- frontend/src/components/DoctorCard/DoctorCard.tsx
+++ frontend/src/components/DoctorCard/DoctorCard.tsx
@@ -1,6 +1,12 @@
 import styles from "./DoctorCard.module.scss";
+import doc from "../../assets/DocDash/doc.png";
+import { Toast } from "primereact/toast";
+import { useRef, useState } from "react";
 interface DoctorCardProps {
+  isSelected: boolean;
+  onClick: (id: number) => void;
   doctor: {
+    id: number;
     fullName: string;
     avatar: string;
     price: number;
@@ -8,13 +14,38 @@ interface DoctorCardProps {
   };
 }
 
-function DoctorCard({ doctor }: DoctorCardProps) {
+function DoctorCard({ doctor, isSelected, onClick }: DoctorCardProps) {
+  const [selectedDoctorId, setSelectedDoctorId] = useState<number | null>(null);
+  const toast = useRef<Toast>(null);
+  const show = () => {
+    toast.current?.show({
+      severity: "info",
+      summary: "Info",
+      detail: "Doctor Selected",
+    });
+  };
+  const handleCardClick = () => {
+    onClick(doctor.id);
+    setSelectedDoctorId(doctor.id);
+    toast.current?.show({
+      severity: "info",
+      summary: "Info",
+      detail: `Doctor  Selected`,
+    });
+  };
+  console.log(doctor.id);
   return (
-    <div className={styles.doctorCard}>
-      {/* <img src={doctor.avatar} alt={doctor.fullName} />
-      <p>{doctor.fullName}</p>
-      <p>Price: {doctor.price}</p>
-      <p>Specialty: {doctor.specialty}</p> */}
+    <div className={styles.doctorCard} onClick={handleCardClick}>
+      <div>
+        <Toast ref={toast} />
+        <img className={styles.doctorImg} src={doc} alt={doctor.fullName} />
+      </div>
+
+      <div className={styles.doctorCardTop}>
+        <div className={styles.docName}>{doctor.fullName}</div>
+        <div>Price: {doctor.price}</div>
+        <div>Speciality: {doctor.specialty}</div>
+      </div>
     </div>
   );
 }
