8fbe101daf869cd13b037f7c833411c118812bbb
MoeShoumar <mks38@mail.aub.edu>
2023-05-05 22:24:35 +0300

    <frontend> added handleselect for search in chats

--- frontend/src/components/Chat/Search.tsx
+++ frontend/src/components/Chat/Search.tsx
@@ -39,6 +39,42 @@ const Search = () => {
       setErr(true);
     }
   };
+  const handleKey = (e: React.KeyboardEvent<HTMLInputElement>): void => {
+    e.code === "Enter" && handleSearch();
+  };
+  const handleSelect = async (): Promise<void> => {
+    //check whether the group(chats in firestore) exists, if not create
+    const combinedId =
+      currentUser.uid > user!.uid
+        ? currentUser.uid + user!.uid
+        : user!.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedId));
+
+      if (!res.exists()) {
+        //create a chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+        //create user chats
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user!.uid,
+            displayName: user!.displayName,
+            photoURL: user!.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user!.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (err) {}
 
     setUser(null);
     setUsername("");
