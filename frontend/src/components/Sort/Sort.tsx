2b24779c1e7b884c54567fa23b70f32159e319de
MoeShoumar <mks38@mail.aub.edu>
2023-05-05 22:26:59 +0300

    <frontend> added handleSpecialtyChange for sort in chats component

--- frontend/src/components/Sort/Sort.tsx
+++ frontend/src/components/Sort/Sort.tsx
@@ -21,6 +21,32 @@ export default function SortDoctor({ onChange }: SortBySpecialtyProps) {
   const [selectedCategory, setSelectedCategory] = useState<Category | null>(
     null
   );
+  const category: Category[] = [
+    { name: "Cardiology", code: "NY", icon: "pi pi-heart" },
+    { name: "Virology", code: "RM", icon: "pi pi-virus" },
+    { name: "Neurology", code: "LDN", icon: "pi pi-brain" },
+    { name: "Chronic-Pain", code: "IST", icon: "pi pi-thermometer" },
+  ];
+  const handleSpecialtyChange = (selectedCategory: Category | null) => {
+    if (selectedCategory) {
+      const filterFn = (doctor: Doctor) =>
+        doctor.specialty === selectedCategory.name;
+      onChange(filterFn);
+    } else {
+      onChange(() => true);
+    }
+    setSelectedCategory(selectedCategory);
+  };
+  const categoryTemplate = (option: Category) => {
+    return (
+      <div className="p-grid p-nogutter p-align-center">
+        <div className="p-col">
+          <i className={option.icon} />
+        </div>
+        <div className="p-col">{option.name}</div>
+      </div>
+    );
+  };
 
   return (
     <div className="card flex justify-content-center">
