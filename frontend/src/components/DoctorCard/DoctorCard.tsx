e4307f667f8846ecfe8291a6ef51e4e364ffa2e7
MoeShoumar <mks38@mail.aub.edu>
2023-05-06 23:51:37 +0300

    <frontend>added useeffect to fetch doctor info

--- frontend/src/components/DoctorCard/DoctorCard.tsx
+++ frontend/src/components/DoctorCard/DoctorCard.tsx
@@ -1,22 +1,35 @@
 import styles from "./DoctorCard.module.scss";
 import doc from "../../assets/DocDash/doc.png";
 import { Toast } from "primereact/toast";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import { base_url } from "../../API/API";
+import { API } from "../../../src/API/API";
 interface DoctorCardProps {
   isSelected: boolean;
   onClick: (id: number) => void;
   doctor: {
     id: number;
-    fullName: string;
-    avatar: string;
-    price: number;
-    specialty: string;
+    displayName: string;
+    imageUrl: string;
+    price?: number;
+    specialty?: string;
   };
+}
+
+interface Doctor {
+  id: number;
+  displayName: string;
+  price?: number;
+  specialty?: string;
+  imageUrl: string;
 }
 
 function DoctorCard({ doctor, isSelected, onClick }: DoctorCardProps) {
   const [selectedDoctorId, setSelectedDoctorId] = useState<number | null>(null);
   const toast = useRef<Toast>(null);
+  const [doctors, setDoctors] = useState<Doctor[]>([]);
+  const token = localStorage.getItem("token");
+
   const show = () => {
     toast.current?.show({
       severity: "info",
@@ -24,6 +37,14 @@ function DoctorCard({ doctor, isSelected, onClick }: DoctorCardProps) {
       detail: "Doctor Selected",
     });
   };
+
+  useEffect(() => {
+    API.getAPI(`${base_url}patient/getdoctors`, token!).then((response) => {
+      const doctors = response;
+      setDoctors(doctors);
+      console.log(response);
+    });
+  }, []);
   const handleCardClick = () => {
     onClick(doctor.id);
     setSelectedDoctorId(doctor.id);
@@ -33,16 +54,16 @@ function DoctorCard({ doctor, isSelected, onClick }: DoctorCardProps) {
       detail: `Doctor  Selected`,
     });
   };
-  console.log(doctor.id);
+
   return (
     <div className={styles.doctorCard} onClick={handleCardClick}>
       <div>
         <Toast ref={toast} />
-        <img className={styles.doctorImg} src={doc} alt={doctor.fullName} />
+        <img className={styles.doctorImg} src={doc} alt={doctor.imageUrl} />
       </div>
 
       <div className={styles.doctorCardTop}>
-        <div className={styles.docName}>{doctor.fullName}</div>
+        <div className={styles.docName}>{doctor.displayName}</div>
         <div>Price: {doctor.price}</div>
         <div>Speciality: {doctor.specialty}</div>
       </div>
