2fab6f5f203d9e0d0e56c91cef91aac10f715c4a
MoeShoumar <mks38@mail.aub.edu>
2023-05-07 23:46:53 +0300

    <frontend> Cleaned and fixed doctor card component

--- frontend/src/components/DoctorCard/DoctorCard.tsx
+++ frontend/src/components/DoctorCard/DoctorCard.tsx
@@ -6,9 +6,9 @@ import { base_url } from "../../API/API";
 import { API } from "../../../src/API/API";
 interface DoctorCardProps {
   isSelected: boolean;
-  onClick: (id: number) => void;
+  onClick: (_id: number) => void;
   doctor: {
-    id: number;
+    _id: number;
     displayName: string;
     imageUrl: string;
     price?: number;
@@ -16,55 +16,33 @@ interface DoctorCardProps {
   };
 }
 
-interface Doctor {
-  id: number;
-  displayName: string;
-  price?: number;
-  specialty?: string;
-  imageUrl: string;
-}
-
 function DoctorCard({ doctor, isSelected, onClick }: DoctorCardProps) {
-  const [selectedDoctorId, setSelectedDoctorId] = useState<number | null>(null);
   const toast = useRef<Toast>(null);
-  const [doctors, setDoctors] = useState<Doctor[]>([]);
-  const token = localStorage.getItem("token");
 
-  const show = () => {
-    toast.current?.show({
-      severity: "info",
-      summary: "Info",
-      detail: "Doctor Selected",
-    });
-  };
-
-  useEffect(() => {
-    API.getAPI(`${base_url}patient/getdoctors`, token!).then((response) => {
-      const doctors = response;
-      setDoctors(doctors);
-      console.log(response);
-    });
-  }, []);
   const handleCardClick = () => {
-    onClick(doctor.id);
-    setSelectedDoctorId(doctor.id);
+    onClick(doctor._id);
+    isSelected = true;
     toast.current?.show({
       severity: "info",
       summary: "Info",
       detail: `Doctor  Selected`,
     });
   };
-
+  console.log(doctor._id);
   return (
     <div className={styles.doctorCard} onClick={handleCardClick}>
       <div>
         <Toast ref={toast} />
-        <img className={styles.doctorImg} src={doc} alt={doctor.imageUrl} />
+        <img
+          className={styles.doctorImg}
+          src={doctor.imageUrl}
+          alt={doctor.imageUrl}
+        />
       </div>
 
       <div className={styles.doctorCardTop}>
         <div className={styles.docName}>{doctor.displayName}</div>
-        <div>Price: {doctor.price}</div>
+        <div>Price: {doctor.price}$</div>
         <div>Speciality: {doctor.specialty}</div>
       </div>
     </div>
