70bd23f23ab5a6b2917ee1645c3e5871248d475e
MoeShoumar <mks38@mail.aub.edu>
2023-05-08 00:00:22 +0300

    <frontedn> linked patients table

--- frontend/src/components/PatientsTable/PatientsTable.tsx
+++ frontend/src/components/PatientsTable/PatientsTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, ReactNode } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
@@ -6,51 +6,74 @@ import { Dialog } from "primereact/dialog";
 import { Rating } from "primereact/rating";
 import { Tag } from "primereact/tag";
 import "primeicons/primeicons.css";
-import { ProductService } from "../service/PatientList";
 /* <DataTable value={products} size={size} tableStyle={{ minWidth: '50rem' }}> */
+import { useNavigate } from "react-router-dom";
 
 interface Product {
+  [x: string]: ReactNode;
   id: string;
-  code: string;
-  name: string;
-  description: string;
-  image?: any;
-  inventoryStatus?: any;
+  displayName: string;
+  description?: string;
+  imageUrl?: any;
+  status?: any;
 }
 
-export default function TemplateDemo() {
-  const [products, setProducts] = useState<Product[]>([]);
+interface Consultation {
+  id: string;
+  patientId: string;
+  doctorId: string;
+  date: string;
+  dignosis: string;
+  treatmentPlan: string;
+  symptoms: string;
+  status: string;
+}
+interface TableComponentProps {
+  patients: Product[] | undefined;
+  consultations: Consultation[] | undefined;
+}
 
+export default function PatientsTable(props: TableComponentProps) {
+  const { patients, consultations } = props;
+  const [products, setProducts] = useState<Product[]>([]);
+  const navigate = useNavigate();
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const [visible, setVisible] = useState(false);
   const [editingResponse, setEditingResponse] = useState(false);
 
-  useEffect(() => {
-    ProductService.getProductsMini().then((data) => setProducts(data));
-  }, []);
-
   const imageBodyTemplate = (product: Product) => {
     return (
       <img
-        src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`}
-        alt={product.image}
+        src={product.imageUrl}
+        alt={product.imageUrl}
         className="w-6rem shadow-2 border-round"
       />
     );
   };
 
-  const statusBodyTemplate = (product: Product) => {
+  // const statusBodyTemplate = (product: Product) => {
+  //   return (
+  //     <Tag
+  //       value={product.inventoryStatus}
+  //       severity={getSeverity(product)}
+  //     ></Tag>
+  //   );
+  // };
+  const consultationBodyTemplate = (patient: Product) => {
+    const patientConsultations = consultations?.filter(
+      (consultations) => consultations.patientId === patient.id
+    );
     return (
-      <Tag
-        value={product.inventoryStatus}
-        severity={getSeverity(product)}
-      ></Tag>
+      <ul>
+        {patientConsultations?.map((consultation) => (
+          <li key={consultation.id}>{consultation.status}</li>
+        ))}
+      </ul>
     );
   };
-  const historyTemplate = (product: Product) => {
+  const historyTemplate = (patient: Product) => {
     const handleClick = () => {
-      // add action here
-      console.log("Medical history clicked for:", product.name);
+      navigate(`/doctor-view/${patient._id}`);
     };
 
     return (
@@ -61,8 +84,8 @@ export default function TemplateDemo() {
       />
     );
   };
-  const getSeverity = (product: Product) => {
-    switch (product.inventoryStatus) {
+  const getSeverity = (patient: Product) => {
+    switch (patient.status) {
       case "Validated":
         return "success";
 
@@ -73,6 +96,20 @@ export default function TemplateDemo() {
         return null;
     }
   };
+
+  const mergedData = patients?.map((patient) => {
+    const matchingConsultations = consultations?.filter(
+      (consultation) => consultation.patientId === patient.id
+    );
+    const consultationStatus =
+      matchingConsultations!.length > 0 ? "Pending" : "Completed";
+
+    return {
+      ...patient,
+      consultations: matchingConsultations,
+      consultationStatus: consultationStatus,
+    };
+  });
 
   const header = (
     <div className="flex flex-wrap align-items-center justify-content-between gap-2">
@@ -101,7 +138,14 @@ export default function TemplateDemo() {
       <Button label="Submit" icon="pi pi-check" className="p-button-success" />
     </div>
   );
-
+  const statusBodyTemplate = (rowData: Product) => {
+    return (
+      <Tag
+        value={rowData.consultationStatus}
+        severity={getSeverity(rowData)}
+      ></Tag>
+    );
+  };
   const dialogContent = (
     <div className="p-grid">
       <div className="p-col-12 p-md-6">
@@ -130,40 +174,51 @@ export default function TemplateDemo() {
     </div>
   );
   const handleRowSelect = (event: any) => {
-    const selectedProduct = event.data;
-    if (selectedProduct && selectedProduct.inventoryStatus === "pending") {
-      setSelectedProduct(selectedProduct);
+    const selectedPatient = event.data;
+    if (selectedPatient.consultationStatus === "Pending") {
+      setSelectedProduct(selectedPatient);
       setVisible(true);
     }
   };
-  // const history = (rowData) => {
-  //   return (
-  //     <div className="flex align-items-center gap-2">
-  //       <img
-  //         alt="flag"
-  //         src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png"
-  //         className={`flag flag-${rowData.country.code}`}
-  //         style={{ width: "24px" }}
-  //       />
-  //       <span>{rowData.country.name}</span>
-  //     </div>
-  //   );
-  // };
 
   return (
     <div className="card">
       <DataTable
-        value={products}
+        value={mergedData}
         header={header}
         tableStyle={{ minWidth: "60rem" }}
         onRowSelect={handleRowSelect}
         selectionMode="single"
         metaKeySelection={false}
       >
-        <Column header="Image" body={(rowData) => rowData.image}></Column>
-        <Column field="First Name" header="First Name"></Column>
-        <Column field="Last Name" header="Last name"></Column>
-        <Column header="Status" body={statusBodyTemplate}></Column>
+        <Column
+          header="Image"
+          body={(rowData) => (
+            <img
+              src={rowData.imageUrl}
+              alt={rowData.displayName}
+              className="w-6rem shadow-2 border-round"
+            />
+          )}
+        ></Column>
+        <Column
+          body={(rowData) => {
+            const fullName = rowData.displayName.split(" ");
+            return fullName[0];
+          }}
+          header="First Name"
+        ></Column>
+        <Column
+          body={(rowData) => {
+            const fullName = rowData.displayName.split(" ");
+            return fullName[fullName.length - 1];
+          }}
+          header="Last name"
+        ></Column>
+        <Column
+          header="Cosnultation Status"
+          body={(rowData) => statusBodyTemplate(rowData)}
+        ></Column>
         <Column header="Medical History" body={historyTemplate}></Column>
       </DataTable>
       <Dialog
@@ -178,3 +233,17 @@ export default function TemplateDemo() {
     </div>
   );
 }
+
+// const history = (rowData) => {
+//   return (
+//     <div className="flex align-items-center gap-2">
+//       <img
+//         alt="flag"
+//         src="https://primefaces.org/cdn/primereact/images/flag/flag_placeholder.png"
+//         className={`flag flag-${rowData.country.code}`}
+//         style={{ width: "24px" }}
+//       />
+//       <span>{rowData.country.name}</span>
+//     </div>
+//   );
+// };
