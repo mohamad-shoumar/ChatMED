2dfa5bb75e89cf8147997bf5a67d330dc99c3bc8
MoeShoumar <mks38@mail.aub.edu>
2023-05-08 23:58:03 +0300

    <frontend>added header to landing page

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,4 +1,8 @@
-export {};
+import styles from "../../styles/LandingPage/LandingPage.module.scss";
+import logo1 from "../../assets/navbar/logo1.png";
+import mini from "../../assets/LandingPage/mini.png";
+import watch from "../../assets/LandingPage/watch.png";
+import doc from "../../assets/LandingPage/doc.png";
 
 // // scroll effect
 // import { useState, useEffect } from "react";
@@ -27,3 +31,40 @@ export {};
 //     </div>
 //   );
 // }
+import { useNavigate } from "react-router-dom";
+
+const LandingPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <div className={styles.main_container}>
+        <div className={styles.navbar}>
+          <div className={styles.main_logo}>
+            <img id="logo" src={logo1} alt="" />
+            <h5 className={styles.logo_head}>ChatMED</h5>
+          </div>
+
+          <div className={styles.nav_buttons}>
+            <button
+              onClick={() => {
+                navigate("/authentication");
+              }}
+              className={styles.but_main}
+            >
+              LOG IN
+            </button>
+            <button
+              onClick={() => {
+                navigate("/authentication");
+              }}
+              className={styles.but_main}
+            >
+              JOIN US <i className="fa-solid fa-arrow-right"></i>
+            </button>
+          </div>
+
+  );
+};
+
+export default LandingPage;
