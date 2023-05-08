a53c67f26464d0f00834f99b95774daef53a5259
MoeShoumar <mks38@mail.aub.edu>
2023-05-08 23:50:58 +0300

    <backend> create routes for getResponse

--- backend/src/routes/response.routes.ts
+++ backend/src/routes/response.routes.ts
@@ -3,8 +3,12 @@ const router: Router = Router();
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { patientMiddleware } from "../middlewares/patient.middleware";
 import { doctorMiddleware } from "../middlewares/doctor.middleware";
-import { responseByChat } from "../controllers/response.controllers";
+import {
+  responseByChat,
+  getResponse,
+} from "../controllers/response.controllers";
 
 router.post("/", authMiddleware, responseByChat);
+router.get("/", authMiddleware, getResponse);
 
 export default router;
