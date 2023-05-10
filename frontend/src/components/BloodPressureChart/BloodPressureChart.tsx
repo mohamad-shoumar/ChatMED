faf9ff771a9be2f66b87901eb935fe29218dc7db
MoeShoumar <mks38@mail.aub.edu>
2023-05-10 23:47:32 +0300

    <frontend>added bloodpressure chat

---
+++ frontend/src/components/BloodPressureChart/BloodPressureChart.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { Chart } from "primereact/chart";
+
+export default function BasicDemo() {
+  const [chartData, setChartData] = useState({});
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {
+    const data = {
+      labels: ["Q1", "Q2", "Q3", "Q4"],
+      datasets: [
+        {
+          label: "Sales",
+          data: [540, 325, 702, 620],
+          backgroundColor: [
+            "rgba(255, 159, 64, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+          ],
+          borderColor: [
+            "rgb(255, 159, 64)",
+            "rgb(75, 192, 192)",
+            "rgb(54, 162, 235)",
+            "rgb(153, 102, 255)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    };
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    };
+
+    setChartData(data);
+    setChartOptions(options);
+  }, []);
+
+  return (
+    <div className="card">
+      <Chart type="bar" data={chartData} options={chartOptions} />
+    </div>
+  );
+}
