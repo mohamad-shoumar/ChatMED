e68ca3dc6aad9ed959dc943244bad7f892900e1f
MoeShoumar <mks38@mail.aub.edu>
2023-05-10 15:16:14 +0300

    <backend> Iterated the advice by ChatGPT api to

--- backend/src/controllers/advice.controllers.ts
+++ backend/src/controllers/advice.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { Configuration, OpenAIApi } from "openai";
 import MedicalHistory from "../models/MedicalHistoryModel";
+import { log } from "console";
 import Advice from "../models/AdviceModel";
 
 export const adviceByChat = async (req: Request, res: Response) => {
@@ -10,8 +11,6 @@ export const adviceByChat = async (req: Request, res: Response) => {
       apiKey: x,
     });
     const openai = new OpenAIApi(configuration);
-    console.log(x);
-    console.log(process.env.OPEN_AI_KEY);
 
     const patientId = req.body.user.id;
     const retrievedMedicalhistory = await MedicalHistory.findOne({
@@ -24,23 +23,9 @@ export const adviceByChat = async (req: Request, res: Response) => {
       max_tokens: 1000,
       n: 1,
     });
-    console.log(response.data);
-    console.log(retrievedMedicalhistory);
 
-    const responseText = response.data.choices[0].text?.trim();
-    if (!responseText) {
-      throw new Error("No response text found.");
-    }
-    const responseData = JSON.parse(response.data.choices[0].text.trim());
-    console.log(responseData);
+    const responseData = response.data.choices[0].text.replace(/^\s+/, "");
 
-    const adviceModel = new Advice({
-      advice: JSON.stringify(responseData),
-      medicalHistory: retrievedMedicalhistory,
-      user: patientId,
-    });
-
-    await adviceModel.save();
     return res.status(200).json({
       message: "success",
       data: responseData,
@@ -58,15 +43,7 @@ const generatePrompt = (retrievedMedicalhistory: any) => {
         advice: advice
         ------------------------------
         Given the above information,
-          patient x has the following medical history: ${retrievedMedicalhistory}.
-         provide a personalized advice for the patient, the advice should be related to the medical history of the patient, it must be between 20 and 50 words and be in the form of talking directley to the patient.
-         Reurtn only a JSON parsable object with the following schema (Do not include any explanation before or after the JSON object):
-         const Schema = mongoose.Schema;
-const AdviceSchema = new Schema({user:{type: Schema.Types.ObjectId,ref:"User",},medicalHistory: {type: Schema.Types.ObjectId,ref: "MedicalHistory",
-  },
-  advice: {
-    type: String,
-    required: true,
-  },
-});`;
+          the patient has the following medical history: ${retrievedMedicalhistory}.
+         provide a personalized advice for the patient, the advice should be related to the medical history of the patient, it must be between 20 and 50 words and be in the form of talking directley to the patient.It must also be smart and not sound like a generic advice.
+        `;
 };
