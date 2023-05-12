dc7be981355813824612bc1fedb50ee11cd9a4a4
MoeShoumar <mks38@mail.aub.edu>
2023-05-12 16:31:55 +0300

    <frontend> added patient table

--- frontend/src/components/PatientsTable/PatientsTable.tsx
+++ frontend/src/components/PatientsTable/PatientsTable.tsx
@@ -5,10 +5,24 @@ import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
 import { Rating } from "primereact/rating";
 import { Tag } from "primereact/tag";
+import { API } from "../../API/API";
+
+import { base_url } from "../../API/API";
+
 import "primeicons/primeicons.css";
 /* <DataTable value={products} size={size} tableStyle={{ minWidth: '50rem' }}> */
 import { useNavigate } from "react-router-dom";
 
+interface Consultation {
+  id: string;
+  patient: string;
+  doctor: string;
+  date: string;
+  diagnosis: string;
+  treatmentPlan: string;
+  symptoms: string;
+  status: string;
+}
 interface Product {
   [x: string]: ReactNode;
   id: string;
@@ -16,18 +30,9 @@ interface Product {
   description?: string;
   imageUrl?: any;
   status?: any;
+  role: any | undefined;
 }
 
-interface Consultation {
-  id: string;
-  patientId: string;
-  doctorId: string;
-  date: string;
-  dignosis: string;
-  treatmentPlan: string;
-  symptoms: string;
-  status: string;
-}
 interface TableComponentProps {
   patients: Product[] | undefined;
   consultations: Consultation[] | undefined;
@@ -36,32 +41,47 @@ interface TableComponentProps {
 export default function PatientsTable(props: TableComponentProps) {
   const { patients, consultations } = props;
   const [products, setProducts] = useState<Product[]>([]);
+
   const navigate = useNavigate();
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const [visible, setVisible] = useState(false);
   const [editingResponse, setEditingResponse] = useState(false);
+  const [consultation, setConsultations] = useState<Consultation[]>([]);
+  const token = localStorage.getItem("token");
+
+  console.log("Patients", patients);
+  console.log("Consultation", consultations);
 
   const imageBodyTemplate = (product: Product) => {
     return (
       <img
         src={product.imageUrl}
         alt={product.imageUrl}
-        className="w-6rem shadow-2 border-round"
+        className="w-6rem shadow-2 border-rounded-half"
       />
     );
   };
+  const handleRowSelect = (event: any) => {
+    const selectedPatient = event.data;
+    console.log("selectedPatient", selectedPatient);
 
-  // const statusBodyTemplate = (product: Product) => {
-  //   return (
-  //     <Tag
-  //       value={product.inventoryStatus}
-  //       severity={getSeverity(product)}
-  //     ></Tag>
-  //   );
-  // };
+    const selectedPatientConsultations = consultations?.filter(
+      (consultation) => consultation.patient === selectedPatient._id
+    );
+
+    if (
+      selectedPatientConsultations &&
+      selectedPatientConsultations.length > 0 &&
+      selectedPatientConsultations[0].status === "pending"
+    ) {
+      setSelectedProduct(selectedPatient);
+      setVisible(true);
+    }
+  };
+
   const consultationBodyTemplate = (patient: Product) => {
     const patientConsultations = consultations?.filter(
-      (consultations) => consultations.patientId === patient.id
+      (consultations) => consultations.patient === patient._id
     );
     return (
       <ul>
@@ -71,9 +91,10 @@ export default function PatientsTable(props: TableComponentProps) {
       </ul>
     );
   };
+
   const historyTemplate = (patient: Product) => {
     const handleClick = () => {
-      navigate(`/doctor-view/${patient._id}`);
+      navigate(`/doctorview`);
     };
 
     return (
@@ -99,8 +120,10 @@ export default function PatientsTable(props: TableComponentProps) {
 
   const mergedData = patients?.map((patient) => {
     const matchingConsultations = consultations?.filter(
-      (consultation) => consultation.patientId === patient.id
+      (consultation) => consultation.patient === patient._id
     );
+    console.log("Matching Consultations", matchingConsultations);
+
     const consultationStatus =
       matchingConsultations!.length > 0 ? "Pending" : "Completed";
 
@@ -114,7 +137,6 @@ export default function PatientsTable(props: TableComponentProps) {
   const header = (
     <div className="flex flex-wrap align-items-center justify-content-between gap-2">
       <span className="text-xl text-900 font-bold">My Patients</span>
-      <Button icon="pi pi-refresh" rounded raised />
     </div>
   );
   const handleEditClick = () => {
@@ -138,6 +160,10 @@ export default function PatientsTable(props: TableComponentProps) {
       <Button label="Submit" icon="pi pi-check" className="p-button-success" />
     </div>
   );
+  // const statusBodyTemplate = (product: Product) => {
+  //   console.log("PRoduct", product);
+  //   return <Tag value={product.status} severity={getSeverity(product)}></Tag>;
+  // };
   const statusBodyTemplate = (rowData: Product) => {
     return (
       <Tag
@@ -146,43 +172,67 @@ export default function PatientsTable(props: TableComponentProps) {
       ></Tag>
     );
   };
-  const dialogContent = (
+
+  console.log("Selected Product", selectedProduct);
+  const dialogContent = selectedProduct && (
     <div className="p-grid">
       <div className="p-col-12 p-md-6">
         <div className="card">
           <h5>Symptoms</h5>
           <hr />
+          {/* <p>{selectedProduct?.role.consultations[0].symptoms}</p> */}
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            suscipit massa auctor lacus sodales faucibus.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est itaque
+            expedita beatae a sint minus id odio rerum. Odio tempora quae
+            tempore cumque minus libero reprehenderit, nesciunt veritatis eos
+            debitis.
           </p>
         </div>
       </div>
-      {editingResponse ? (
-        <div className="p-col-12 p-md-6">
-          <div className="card">
-            <h5>Response</h5>
-            <hr />
-            <textarea
-              value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit massa auctor lacus sodales faucibus."
-              onChange={(e) => {}}
-              className="w-full p-2 border rounded"
-            ></textarea>
-          </div>
+      <div className="p-col-12 p-md-6">
+        <div className="card">
+          <h5>Diagnosis</h5>
+          <hr />
+          {/* <p>{selectedProduct?.diagnosis}</p> */}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est itaque
+            expedita beatae a sint minus id odio rerum. Odio tempora quae
+            tempore cumque minus libero reprehenderit, nesciunt veritatis eos
+            debitis.
+          </p>
         </div>
-      ) : null}
+      </div>
+      <div className="p-col-12 p-md-6">
+        <div className="card">
+          <h5>Treatment Plan</h5>
+          <hr />
+          {/* <p>{selectedProduct.treatmentPlan}</p> */}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est itaque
+            expedita beatae a sint minus id odio rerum. Odio tempora quae
+            tempore cumque minus libero reprehenderit, nesciunt veritatis eos
+            debitis.
+          </p>
+        </div>
+      </div>
     </div>
   );
-  const handleRowSelect = (event: any) => {
-    const selectedPatient = event.data;
-    if (selectedPatient.consultationStatus === "Pending") {
-      setSelectedProduct(selectedPatient);
-      setVisible(true);
-    }
-  };
+
+  console.log("Comeplete Consultation", consultation);
+  console.log(mergedData);
+  console.log(mergedData?.[0].displayName);
+  console.log(mergedData?.[1].displayName);
+  console.log("Merged Data", mergedData?.[0]);
 
   return (
     <div className="card">
+      <style>
+        {`
+        .custom-datatable tbody tr:hover {
+          background-color: rgba(1, 184, 226, 0.15)  !important;; 
+        }
+      `}
+      </style>
       <DataTable
         value={mergedData}
         header={header}
@@ -190,6 +240,7 @@ export default function PatientsTable(props: TableComponentProps) {
         onRowSelect={handleRowSelect}
         selectionMode="single"
         metaKeySelection={false}
+        className="custom-datatable"
       >
         <Column
           header="Image"
@@ -197,27 +248,30 @@ export default function PatientsTable(props: TableComponentProps) {
             <img
               src={rowData.imageUrl}
               alt={rowData.displayName}
-              className="w-6rem shadow-2 border-round"
+              className="w-6rem shadow-2 border-round border-rounded-half"
             />
           )}
         ></Column>
+
         <Column
           body={(rowData) => {
+            console.log("rowwwwjhsdf", rowData);
             const fullName = rowData.displayName.split(" ");
-            return fullName[0];
+            return fullName;
           }}
           header="First Name"
         ></Column>
         <Column
           body={(rowData) => {
-            const fullName = rowData.displayName.split(" ");
+            console.log(rowData);
+            const fullName = rowData[0].displayName.split(" ");
             return fullName[fullName.length - 1];
           }}
           header="Last name"
         ></Column>
         <Column
-          header="Cosnultation Status"
-          body={(rowData) => statusBodyTemplate(rowData)}
+          header="Consultation Status"
+          body={(rowData) => statusBodyTemplate(rowData.consultationStatus)}
         ></Column>
         <Column header="Medical History" body={historyTemplate}></Column>
       </DataTable>
@@ -234,7 +288,91 @@ export default function PatientsTable(props: TableComponentProps) {
   );
 }
 
-// const history = (rowData) => {
+// const dialogContent = (
+//   <div className="p-grid">
+//     <div className="p-col-12 p-md-6">
+//       <div className="card">
+//         <h5>Symptoms</h5>
+//         <hr />
+//         <p>
+//           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+//           suscipit massa auctor lacus sodales faucibus.
+//         </p>
+//       </div>
+//     </div>
+//     {editingResponse ? (
+//       <div className="p-col-12 p-md-6">
+//         <div className="card">
+//           <h5>Response</h5>
+//           <hr />
+//           <textarea
+//             value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit massa auctor lacus sodales faucibus."
+//             onChange={(e) => {}}
+//             className="w-full p-2 border rounded"
+//           ></textarea>
+//         </div>
+//       </div>
+//     ) : null}
+//   </div>
+// );
+
+// const handleRowSelect = async (event: any) => {
+//   const selectedPatient = event.data;
+//   if (selectedPatient.consultationStatus === "Pending") {
+//     try {
+//       const responseConsultation = await API.getAPI(
+//         `${base_url}response`,
+//         token!
+//       );
+//       console.log("Response consultation", responseConsultation);
+//       setConsultation(responseConsultation.response);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//     setSelectedProduct(selectedPatient);
+//     setVisible(true);
+//   }
+// };
+
+// const consultationBodyTemplate = (patient: Product) => {
+//   const patientConsultations = consultations?.filter(
+//     (consultations) => consultations.patient === patient._id
+//   );
+//   if (patientConsultations && patientConsultations.length > 0) {
+//     const latestConsultation = patientConsultations[0];
+//     const handleClick = () => {
+//       if (latestConsultation.status === "pending") {
+//         setSelectedProduct(patient);
+//         setVisible(true);
+//       }
+//     };
+//     return (
+//       <div onClick={handleClick} style={{ cursor: "pointer" }}>
+//         {latestConsultation.status === "pending" ? (
+//           <span>Pending</span>
+//         ) : (
+//           <span>Completed</span>
+//         )}
+//       </div>
+//     );
+//   }
+//   return null;
+// };
+
+// const history = (rowData: {
+//   country: {
+//     code: any;
+//     name:
+//       | string
+//       | number
+//       | boolean
+//       | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+//       | React.ReactFragment
+//       | React.ReactPortal
+//       | null
+//       | undefined;
+//   };
+// }) => {
 //   return (
 //     <div className="flex align-items-center gap-2">
 //       <img
